b5b0129ad9bc2f7a366ed2a6993c948ac10d4ac5
Rory Walker <redrory@gmail.com>
2014-01-15 11:30:43 -0500

    sample user added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,32 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        format.html {redirect_to(@user,
+                                :notice => 'User was successfully create.')}
+        format.json {render :json => @user,
+                                :status => :created, :location => @user}
+
+      else
+        format.html {render :action => "new"}
+        format.json {render :json => @user.errors,
+                      :status => :unprocessable_entity}
+      end
+    end
   end
 
   def welcome
+  end
+
+  def show
+    @user = User.find(params[:id])
+
   end
 end
