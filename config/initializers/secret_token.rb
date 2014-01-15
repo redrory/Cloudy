486ef9b67ec261ba2829c727f490c39fc4a8538d
Rory Walker <redrory@gmail.com>
2014-01-15 09:29:05 -0500

    first cloudy commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloudy::Application.config.secret_token = '662e191cbdcd72c78812247b9f047bfd1a0d75d8672b3e899d1e738dcb09d6bf2d328f47c9c158f9f0e72f93e11549e59a91a076bdb392e0c81ded4c27b3ce4a'
