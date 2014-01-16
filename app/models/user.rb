a88ce6bb1d1ad315651c16bb0bdda6f130953bef
Rory Walker <redrory@gmail.com>
2014-01-16 17:05:04 -0500

    add sch task

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,27 @@
 class User < ActiveRecord::Base
   attr_accessible :address1, :city, :country, :email, :name, :role, :telephoneNo
+
+  def weather
+
+    #get weather forecast
+    count_staff
+    get_weather("kingston")
+    get_mobay_weather
+
+    #get respectively users
+    @kgn_IT = User.where("city = ? AND role = ?", "Kingston", "IT")
+    @mobay_IT = User.where("city = ? AND role = ?", "Montego Bay", "IT")
+    @kgn_office = User.where("city = ? AND role = ?", "Kingston", "Office")
+    @mobay_office = User.where("city = ? AND role = ?", "Montego Bay", "Office")
+
+    #Kgn Push
+    send_office_email(@kgn_weather, @kgn_office, "Kingston")
+    send_it_email(@kgn_weather, @kgn_IT, "kgn" )
+
+    #Mobay Push
+    send_office_email(@mobay_weather, @mobay_office, "Montego Bay")
+    send_it_email(@mobay_weather, @mobay_IT, "Mobay" )
+
+  end
+
 end
