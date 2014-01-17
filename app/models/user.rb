8a8616e53d7cb559fe087cfa7908a36c7ff6bf95
Rory Walker <redrory@gmail.com>
2014-01-16 22:54:52 -0500

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :address1, :city, :country, :email, :name, :role, :telephoneNo
-  include module UsersHelper
+  include UsersHelper
 
 
   def welcome
